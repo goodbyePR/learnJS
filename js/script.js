294226aad5a203c3ecfbef14b38e8f19d5d077f7
VladislavG <prpsmslyt@gmail.com>
2025-11-12 19:48:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let userName = "John";
+let userNumber = 25;
+userNumber = 24;
+
+console.log(userName);
+
+
+let storeName = "Vladislav Store"
+let storeDescription = {
+    budget: 10000,
+    employees: ['Vlad', 'Alex', 'Sveta'],
+    products: {
+        bread: 100,
+        water: 50
+    },
+    open: true
+};
